7754dc581017cb31f7af33669bfbddbbea649283
robson <robson@magario.com>
2021-12-18 15:54:52 +0800

    this version works partially.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -37,14 +38,22 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<String> getJSONData() async {
     var response = await http.get(
       // Encode the url
-        Uri.parse("https://unsplash.com/napi/photos/Q14J2k8VE3U/related"),
+        Uri.parse("https://api.json-generator.com/templates/Xp8zvwDP14dJ/data?access_token=v3srs6i1veetv3b2dolta9shrmttl72vnfzm220z"),
         // Only accept JSON response
-        headers: {"Accept": "application/json"}
-    );
+        headers: {
+      //    HttpHeaders.authorizationHeader: '"access_token":  "v3srs6i1veetv3b2dolta9shrmttl72vnfzm220z"',
+          "Accept": "application/json"
+             }
 
+     //   Uri.parse("https://unsplash.com/napi/photos/Q14J2k8VE3U/related"),
+    // Only accept JSON response
+    //    headers: {"Accept": "application/json"}
+    );
+  print (response.body);
     setState(() {
       // Get the JSON data
-      data = json.decode(response.body)['results'];
+      data = json.decode(response.body.toString());
+      //['_id'];
     });
 
     return "Successfull";
@@ -79,7 +88,8 @@ class _MyHomePageState extends State<MyHomePage> {
     child: Column(
       children: [
         new CachedNetworkImage(
-          imageUrl: item['urls']['small'],
+          imageUrl: item['picture'],
+      //    imageUrl: item['urls']['small'],
           placeholder: (context, url) => new CircularProgressIndicator(),
           errorWidget: (context, url, error) => new Icon(Icons.error),
           fadeOutDuration: new Duration(seconds: 1),
@@ -93,7 +103,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildRow(dynamic item) {
     return ListTile(
       title: Text(
-        item['description'] == null ? '': item['description'],
+        item['name']['last'] == null ? '': item['name']['last'],
+     //   item['description'] == null ? '': item['description'],
       ),
       subtitle: Text("Likes: " + item['likes'].toString()),
     );
